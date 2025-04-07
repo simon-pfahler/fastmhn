5f535ee9c17a7716a4fffcabc2817452336ce8bb
Simon Pfahler <simon.pfahler@physik.uni-regensburg.de>
2025-04-07 08:08:49 +0200

    Add verbose option to approximate gradient calculation

--- src/fastmhn/approx.py
+++ src/fastmhn/approx.py
@@ -5,7 +5,7 @@ from .exact import gradient_and_score
 from .utility import create_pD
 
 
-def approx_gradient(theta, data, max_cluster_size=None):
+def approx_gradient(theta, data, max_cluster_size=None, verbose=False):
     """
     Calculates approximate gradients using the clustering provided by
     `fastmhn.clustering.get_cluster`.
@@ -13,12 +13,15 @@ def approx_gradient(theta, data, max_cluster_size=None):
     `theta`: dxd theta matrix
     `data`: Nxd matrix containing the dataset
     `max_cluster_size`: maximal allowed size for clusters
+    `verbose`: set to `True` to get more output
     """
     d = theta.shape[0]
     if max_cluster_size == None:
         max_cluster_size = d
 
     gradient = np.zeros((d, d))
+    if verbose:
+        print("Indices: gradient")
     for i in range(d):
         # base rate gradients
         columns = get_clusters(theta, i, i, max_cluster_size)[0]
@@ -27,6 +30,8 @@ def approx_gradient(theta, data, max_cluster_size=None):
         )
         key = tuple(sorted(columns))
         gradient[i, i] = g[0, 0]
+        if verbose:
+            print(f"{i}, {i}: {gradient[i,i]} ({len(columns)} events)")
         for j in range(i + 1, d):
             # influence gradients
             columns = get_clusters(theta, i, j, max_cluster_size)[0]
@@ -36,4 +41,7 @@ def approx_gradient(theta, data, max_cluster_size=None):
             key = tuple(sorted(columns))
             gradient[i, j] = g[0, 1]
             gradient[j, i] = g[1, 0]
+            if verbose:
+                print(f"{i}, {j}: {gradient[i,j]} ({len(columns)} events)")
+                print(f"{j}, {i}: {gradient[j,i]} ({len(columns)} events)")
     return gradient
